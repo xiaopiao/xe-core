0ac794467ba2f1da5b1dea85b5c013c5340c04d8
YJSoft <yjsoft@yjsoft.pe.kr>
2015-10-06 18:12:14 +0900

    NOISSUE change indent

--- tests/_support/Step/Acceptance/Login.php
+++ tests/_support/Step/Acceptance/Login.php
@@ -5,29 +5,29 @@ use Page\XEURLBuilder as XEURL;
 class Login extends \AcceptanceTester
 {
 
-    public function loginAsUser($id,$pw)
-    {
-        $I = $this;
-        $I->amOnPage(XEURL::getNotEncodedUrl('act','dispMemberLoginForm'));
+	public function loginAsUser($id,$pw)
+	{
+		$I = $this;
+		$I->amOnPage(XEURL::getNotEncodedUrl('act','dispMemberLoginForm'));
 		$I->submitForm('#fo_member_login', array(
-		     'user_id' => $id,
-		     'password' => $pw
+			'user_id' => $id,
+			'password' => $pw
 		));
-    }
+	}
 
-    public function loginAsAdmin($pw)
-    {
-        $I = $this;
-        $I->amOnPage(XEURL::getNotEncodedUrl('act','dispMemberLoginForm'));
+	public function loginAsAdmin($pw)
+	{
+		$I = $this;
+		$I->amOnPage(XEURL::getNotEncodedUrl('act','dispMemberLoginForm'));
 		$I->submitForm('#fo_member_login', array(
-		     'user_id' => 'admin@admin.net',
-		     'password' => $pw
+			'user_id' => 'admin@admin.net',
+			'password' => $pw
 		));
-    }
+	}
 
-    public function logout()
-    {
-        $I = $this;
-        $I->amOnPage(XEURL::getNotEncodedUrl('act','dispMemberLogout'));
-    }
+	public function logout()
+	{
+		$I = $this;
+		$I->amOnPage(XEURL::getNotEncodedUrl('act','dispMemberLogout'));
+	}
 }
